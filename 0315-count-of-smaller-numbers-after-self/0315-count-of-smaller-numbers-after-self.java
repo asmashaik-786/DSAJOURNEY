47ca43492026abd85b16695d94d32a5f2a6fecc2
shaik Asma <118794824+asmashaik-786@users.noreply.github.com>
2025-06-16 18:22:12 +0530

    Time: 150 ms (35.32%), Space: 64 MB (24.09%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.java
@@ -14,57 +14,56 @@ class Solution {
         {
             pairs[i]=new Pair(nums[i],i);
         }
-        int[]res=new int[nums.length];
-        merge(pairs,0,pairs.length-1,res);
         List<Integer>a1=new ArrayList<>();
-        for(int i:res)
+        for(int i=0;i<nums.length;i++)
         {
-            a1.add(i);
+            a1.add(0);
         }
+        merge(pairs,0,pairs.length-1,a1);
         return a1;
     }
-    static void merge(Pair[]arr,int l,int h,int[]res)
+    static void merge(Pair[]arr,int l,int h,List<Integer>a1)
     {
         if(l>=h)
         {
             return;
         }
         int mid=(l+h)/2;
-        merge(arr,l,mid,res);
-        merge(arr,mid+1,h,res);
-        merging(arr,l,mid,h,res);
+        merge(arr,l,mid,a1);
+        merge(arr,mid+1,h,a1);
+        merging(arr,l,mid,h,a1);
     }
-    static void merging(Pair[]arr,int l,int mid,int h,int[]res)
+    static void merging(Pair[]arr,int l,int mid,int h,List<Integer>a1)
     {
         int i=l;
         int j=mid+1;
         int c=0;
-        ArrayList<Pair>a1=new ArrayList<>();
+        ArrayList<Pair>a2=new ArrayList<>();
         while(i<=mid && j<=h)
         {
             if(arr[i].value<=arr[j].value)
             {
-                res[arr[i].index]+=c;
-                a1.add(arr[i++]);
+                a1.set(arr[i].index,a1.get(arr[i].index)+c);
+                a2.add(arr[i++]);
             }
             else
             {
                 c++;
-                a1.add(arr[j++]);
+                a2.add(arr[j++]);
             }
         }
         while(i<=mid)
         {
-            res[arr[i].index]+=c;
-            a1.add(arr[i++]);
+           a1.set(arr[i].index,a1.get(arr[i].index)+c);
+            a2.add(arr[i++]);
         }
         while(j<=h)
         {
-            a1.add(arr[j++]);
+            a2.add(arr[j++]);
         }
         for(int k=l;k<=h;k++)
         {
-            arr[k]=a1.get(k-l);
+            arr[k]=a2.get(k-l);
         }
     }
 }
