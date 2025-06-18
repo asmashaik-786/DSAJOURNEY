cc148481851b0e28395bb7785178b6d47f5b5752
shaik Asma <118794824+asmashaik-786@users.noreply.github.com>
2025-06-18 14:55:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        int[]ar=new int[n];
+        List<List<Integer>>a1=new ArrayList<>();
+        if(n>9) return a1;
+        for(int i=1;i<=n;i++)
+        {
+            ar[i-1]=i;
+        }
+        Recur(ar,a1,k,0,new ArrayList<>(),n);
+        return a1;
+    }
+    static void Recur(int[]ar,List<List<Integer>>a1,int k,int i,ArrayList<Integer>list,int tar)
+    {
+        if(list.size()==k && tar==0)
+        {
+            a1.add(new ArrayList(list));
+            return;
+        }
+        if(i<ar.length)
+        {
+            if(tar>=ar[i])
+            {
+                list.add(ar[i]);
+                Recur(ar,a1,k,i+1,list,tar-ar[i]);
+                list.remove(list.size()-1);
+                Recur(ar,a1,k,i+1,list,tar);
+            }
+        }
+    }
+}
